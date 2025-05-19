a974d62a3724825508ba6e2ce16fc3ea62b9cf66
missionode <116753080+missionode@users.noreply.github.com>
2025-05-19 21:02:45 +0530

    Update prosperity.js

--- js/prosperity.js
+++ js/prosperity.js
@@ -106,6 +106,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     coinRainTrigger.addEventListener('click', () => {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        
         const numberOfCoins = updateDailyCounter();
         console.log('Coin trigger clicked, creating', numberOfCoins, 'coins.');
 
